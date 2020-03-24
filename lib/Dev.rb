8aabbac7a6ad8c20812ab7264395880af78931e1
joshuaAjack <Mynameisjoshuajack@gmail.com>
2020-03-24 00:22:05 -0700

    outline complete

---
+++ lib/Dev.rb
@@ -0,0 +1,14 @@
+require "Dev/version"
+
+require_relative './Dev/article'
+require_relative './Dev/cli'
+require_relative './Dev/scraper'
+
+require "nokogiri"
+require "open-uri"
+require "pry"
+
+module Dev
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/Dev/version.rb
@@ -0,0 +1,3 @@
+module Dev
+  VERSION = "0.1.0"
+end
